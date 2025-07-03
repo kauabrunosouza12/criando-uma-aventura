78c67bbb0168e45f850b7ba0fcc3e92eef1906bb
kauabrunosouza12 <kaua.jaco.souza@escola.pr.gov.br>
2025-07-03 09:38:14 -0300

    style.css

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener ('click', function(){
         const atual = document.querySelector
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementByld(proximoPasso).classList.add()
     })
 })
